aa73e3a9e212a9a1d4c6f07e8f9d9cebd8640ba2
Leandro Zemborain <leozembo@hotmail.com>
2023-03-30 19:20:59 -0300

    subiendo

---
+++ src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function CartWidget() {
+  return (
+    <div>
+        <i class="bi bi-cart3"></i>
+        0
+    </div>
+  )
+}
+
+export default CartWidget
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import CartWidget from '../CartWidget/CartWidget';
+import './NavBar.css';
+
+function Navegacion() {
+  return (
+      <Navbar bg="light" expand="lg">
+          <Container>
+              <Navbar.Brand href="#home">
+                  <img className="logo" src="logo.jpg" alt="logo" width="100" height="60"  //Si pongo una imagen de la web me la renderiza, pero cuando lo hago local no. Ayuda!
+                    />
+              </Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                  <Nav className="me-auto">
+                      <Nav.Link href="#home">Catálogo</Nav.Link>
+                      <Nav.Link href="#link">Quienes somos</Nav.Link>
+                      <Nav.Link href="#link">Contacto</Nav.Link>
+                  </Nav>
+              </Navbar.Collapse>
+            <CartWidget />
+          </Container>
+      </Navbar>
+  )
+}
+
+export default Navegacion
+
+
+
